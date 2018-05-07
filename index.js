80a2481693a72c1135b656fa8e7fdd58f9d773fb
Valerie Moore <periwinkle17@outlook.com>
2018-05-07 00:54:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 function getFirstSelector(selector) {
-  document.querySelector('div')
+  return document.querySelector(selector)
   }
+  
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+  }
+  
+function increaseRankBy(n) {
+  var newArray = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < newArray.length; i++){
+  var kids = newArray[i].children
+  for (let i = 0; i < kids.length; i++){
+ kids[i].innerHTML = parseInt(kids[i].innerHTML) +n
+  }
+}
+}
+
+function helperFunction(element) {
+    if (element.children.length > 0) {
+    return helperFunction(element.children[0])
+  } else {
+    return element
+  }
+}
+
+function deepestChild() {
+  let element = document.querySelector('#grand-node')
+  return helperFunction(element)
+}
+
+    
