3fd6b4fce32773c00333f3e9c0da54ed8aa9afdb
Valerie Moore <periwinkle17@outlook.com>
2018-05-07 00:33:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function increaseRankBy(n) {
 function helperFunction(element){
   if (element.hasChildNodes()) {
     return helperFunction(element.children[0])
-}else{
+  } else {
 return element
   }
   }
